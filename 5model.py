e7dee810e4458bdbcb8678bd9ee2f909908c714d
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2026-05-11 12:44:35 -0700

    Add files via upload

--- 5model.py
+++ 5model.py
@@ -8,25 +8,25 @@ from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.api import stats 
 
 def plots(data, label):
-#     plot_acf(data, zero = False)
-#     plt.title(label + '\n ACF for Original Values')
-#     plt.savefig('O-' + label + '.png')
-#     plt.close()
-#     plot_acf(abs(data), zero = False)
-#     plt.title(label + '\n ACF for Absolute Values')
-#     plt.savefig('A-' + label + '.png')
-#     plt.close()
-#     qqplot(data, line = 's')
-#     plt.title(label + '\n Quantile-Quantile Plot vs Normal')
-#     plt.savefig('QQ-' + label + '.png')
-#     plt.close()
+    plot_acf(data, zero = False)
+    plt.title(label + '\n ACF for Original Values')
+    plt.savefig('O-' + label + '.png')
+    plt.close()
+    plot_acf(abs(data), zero = False)
+    plt.title(label + '\n ACF for Absolute Values')
+    plt.savefig('A-' + label + '.png')
+    plt.close()
+    qqplot(data, line = 's')
+    plt.title(label + '\n Quantile-Quantile Plot vs Normal')
+    plt.savefig('QQ-' + label + '.png')
+    plt.close()
     print(label)
     print('ACF p-value for Ljung-Box test = ', stats.acorr_ljungbox(data, lags = [5, 10])['lb_pvalue'].values)
     print('Same for absolute values = ', stats.acorr_ljungbox(abs(data), lags = [5, 10])['lb_pvalue'].values)
     print('Jarque-Bera p = ', scipy.stats.jarque_bera(data)[1])
-    # return np.std(data)
+    return np.std(data)
     
-DF = pd.read_excel('data2025-new.xlsx', sheet_name = 'data')
+DF = pd.read_excel('data2025.xlsx', sheet_name = 'data')
 vol = DF['Volatility'].values[1:]
 price = DF['Price'].values
 div = DF['Dividends'].values
@@ -35,25 +35,25 @@ bonds = DF['Bonds'].values[45:]
 intl = DF['International'].values[43:]
 N = 98
 
-# plt.plot(range(1928, 1928 + N), vol)
-# plt.title('Volatility')
-# plt.savefig('vol.png')
-# plt.close()
-# 
-# plt.plot(range(1927, 1928 + N), price)
-# plt.title('Index')
-# plt.savefig('index.png')
-# plt.close()
-# 
-# plt.plot(range(1927, 1928 + N), rates)
-# plt.title('BAA Rate')
-# plt.savefig('baa.png')
-# plt.close()
-# 
-# plt.plot(range(1927, 1928 + N), div)
-# plt.title('Dividends')
-# plt.savefig('div.png')
-# plt.close()
+plt.plot(range(1928, 1928 + N), vol)
+plt.title('Volatility')
+plt.savefig('vol.png')
+plt.close()
+
+plt.plot(range(1927, 1928 + N), price)
+plt.title('Index')
+plt.savefig('index.png')
+plt.close()
+
+plt.plot(range(1927, 1928 + N), rates)
+plt.title('BAA Rate')
+plt.savefig('baa.png')
+plt.close()
+
+plt.plot(range(1927, 1928 + N), div)
+plt.title('Dividends')
+plt.savefig('div.png')
+plt.close()
 
 lvol = np.log(vol)
 total = np.array([np.log(price[k+1] + div[k+1]) - np.log(price[k]) for k in range(N)])
@@ -72,7 +72,7 @@ print('Regression for the log volatility')
 print(RegVol.summary())
 resVol = RegVol.resid
 
-RegRates = OLS(np.diff(np.log(rates))/vol, pd.DataFrame({'const' : 1/vol, 'lag' : np.log(rates)[:-1]/vol, 'vol' : 1})).fit()
+RegRates = OLS(np.diff(np.log(rates))/vol, pd.DataFrame({'const' : 1/vol, 'lag' : np.log(rates)[:-1]/vol})).fit()
 print('Regression for Rates')
 print(RegRates.summary())
 resRates = RegRates.resid
@@ -84,7 +84,7 @@ print(RegIntl.summary())
 resIntl = RegIntl.resid
 
 print('bond returns')
-RegBonds = OLS(normBonds, mainDF[['const', 'duration']].iloc[45:]).fit()
+RegBonds = OLS(normBonds, -np.diff(rates)[45:]/vol[45:]).fit()
 print(RegBonds.summary())
 resBonds = RegBonds.resid
 
