31c92185d059e885d4622e0188e5d36af7787dcd
Andrey Sarantsev <50476640+asarantsev@users.noreply.github.com>
2026-08-14 07:53:57 -0700

    Update risk-spread.py

--- risk-spread.py
+++ risk-spread.py
@@ -18,30 +18,33 @@ lspreads = np.log(spreads)
 total = np.array([np.log(price[k+1] + div[k+1]) - np.log(price[k]) for k in range(N)])
 ntotal = total/vol
 
+# First, for complete picture, autoregression of spreads with lag 1 with stochastic volatility
+# It was really in another piece of code, but we still include this here
 RegSpreads = OLS(spreads[1:]/vol, pd.DataFrame({'const' : 1/vol, 'lag' : spreads[:-1]/vol, 'vol' : 1})).fit()
 print(RegSpreads.summary())
 plots(RegSpreads.resid, 'vol-spreads')
-Reg1 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'vol' : 1, 'spreads' : spreads[:-1]/vol})).fit()
-plots(Reg1.resid, 'returns1')
-Reg2 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'vol' : 1, 'spreads' : spreads[:-1]})).fit()
-plots(Reg2.resid, 'returns2')
-Reg3 = OLS(ntotal, pd.DataFrame({'const' : 1, 'spreads' : spreads[:-1]})).fit()
-plots(Reg3.resid, 'returns3')
-Reg4 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'spreads' : spreads[:-1]})).fit()
-plots(Reg4.resid, 'returns4')
-Reg5 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'spreads' : spreads[:-1]/vol})).fit()
-plots(Reg5.resid, 'returns5')
-Reg6 = OLS(ntotal, pd.DataFrame({'const' : 1, 'spreads' : spreads[:-1]/vol})).fit()
-plots(Reg6.resid, 'returns6')
-RegL1 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'vol' : 1, 'spreads' : lspreads[:-1]/vol})).fit()
-plots(RegL1.resid, 'returns-log1')
-RegL2 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'vol' : 1, 'spreads' : lspreads[:-1]})).fit()
-plots(RegL2.resid, 'returns-log2')
-RegL3 = OLS(ntotal, pd.DataFrame({'const' : 1, 'spreads' : lspreads[:-1]})).fit()
-plots(RegL3.resid, 'returns-log3')
-RegL4 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'spreads' : lspreads[:-1]})).fit()
-plots(RegL4.resid, 'returns-log4')
-RegL5 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'spreads' : lspreads[:-1]/vol})).fit()
-plots(RegL5.resid, 'returns-log5')
-RegL6 = OLS(ntotal, pd.DataFrame({'const' : 1, 'spreads' : lspreads[:-1]/vol})).fit()
-plots(RegL6.resid, 'returns-log6')
+
+# Now many regressions for normalized S&P returns versus spreads
+# in different combinations
+Reg0 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'vol' : 1, 'spreads' : spreads[:-1]/vol})).fit()
+Reg1 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'vol' : 1, 'spreads' : spreads[:-1]})).fit()
+Reg2 = OLS(ntotal, pd.DataFrame({'const' : 1, 'spreads' : spreads[:-1]})).fit()
+Reg3 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'spreads' : spreads[:-1]})).fit()
+Reg4 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'spreads' : spreads[:-1]/vol})).fit()
+Reg5 = OLS(ntotal, pd.DataFrame({'const' : 1, 'spreads' : spreads[:-1]/vol})).fit()
+
+# Regressions for normalized returns versus log spreads
+# in different combinations
+RegL0 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'vol' : 1, 'spreads' : lspreads[:-1]/vol})).fit()
+RegL1 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'vol' : 1, 'spreads' : lspreads[:-1]})).fit()
+RegL2 = OLS(ntotal, pd.DataFrame({'const' : 1, 'spreads' : lspreads[:-1]})).fit()
+RegL3 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'spreads' : lspreads[:-1]})).fit()
+RegL4 = OLS(ntotal, pd.DataFrame({'const' : 1/vol, 'spreads' : lspreads[:-1]/vol})).fit()
+RegL5 = OLS(ntotal, pd.DataFrame({'const' : 1, 'spreads' : lspreads[:-1]/vol})).fit()
+
+# labels
+labels = ['returns-vs-spreads-' + str(k) for k in range(6)] + ['returns-vs-logspreads-' + str(k) for k in range(6)]
+# Now print the output and the analysis of residuals
+for Reg in [Reg0, Reg1, Reg2, Reg3, Reg4, Reg5, RegL0, RegL1, RegL2, RegL3, RegL4, RegL5]:
+  print(Reg.summary())
+  plots(Reg.resid, 'returns-log6')
